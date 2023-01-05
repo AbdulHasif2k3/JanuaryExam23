b470d080b52f74ebd317f8c53f1a39dd9cadfb68
s00224934 <s00224934@LAB-C1002-02>
2023-01-05 09:59:20 +0000

    Add project files.

---
+++ Player.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JanuaryExam23
+{
+    public class Player
+    {
+        string Name;
+        string ResultRecord;
+    }
+}
